2a6931a05afab756494c00b15c723c3c3ae7aae4
Ryan <ryangabryel2005@gmail.com>
2026-05-17 23:10:09 -0300

    funny arrays

--- introductory/parametrization.scala
+++ introductory/parametrization.scala
@@ -6,15 +6,26 @@ class Parametrization(text: String*) {
 }
 
 @main def testez() : Unit = {
+    // Other interesting thing is that the Scala compiler will transform the sentences into invocations of methods
     val valor = Parametrization("Hello World I'm Ryan")
     val greetStrings = new Array[String](3)
 
     greetStrings(0) = "Hello"
     greetStrings(1) = ", "
     greetStrings(2) = "World!\n"
-    
+
+    // What we are doing here is basically, calling a factory method named apply, which creates and return the array
+    val numsNames = Array("zero", "one", "two")
+    // This is the same as
+    val numsNames2 = Array.apply("zero", "one", "two")
+
+    for(i <- 0 to 2) 
+        // To more complex outputs, it's recommended to use {} to output your string or etc
+        println(s"${numsNames(i)} : ${numsNames2(i)}")
+
+
     for(i <- 0 to 2) 
         print(greetStrings(i))
-        
+
     valor.teste()
 }
