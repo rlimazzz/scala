2d478939b866be8e76e808fcf329906361f78b35
Ryan <ryangabryel2005@gmail.com>
2026-05-18 13:50:12 -0300

    operators order things

---
+++ introductory/structures.scala
@@ -0,0 +1,9 @@
+@main def structures() : Unit = {
+    val twoThree = List(2, 3)
+    // cons :: usage 
+    val oneTwoThree = 1 :: twoThree
+    val lista = 1 :: 2 :: 3 :: Nil
+
+    println(lista)
+    println(oneTwoThree)
+}
