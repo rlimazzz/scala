57df402dcbdbbf4c169716c3420134ed161637a9
Ryan <ryangabryel2005@gmail.com>
2026-05-19 09:47:43 -0300

    more theory

--- introductory/arrays.scala
+++ introductory/arrays.scala
@@ -5,13 +5,14 @@
     println(firstArray(1))
     firstArray(1) = 4
     firstArray.foreach(i => if(i == firstArray.last) print(s"$i\n") else print(s"$i,"))
-    firstArray.foreach(i => i + 1)
 
     /* 
+    STRUCTURAL SHARING : 
     The thing in functional programming is that creating copies of
     arrays or any other structure is O(1) because it does not create another
     block of memory like in the imperative programming paradigm, it just points the
     new array with the new elements to the old block of memory
+     BUT THIS IS NOT APPLIED TO Arrays SINCE ARRAYS ARE MUTABLE AND THIS IS ONLY APPLIED TO IMMUTABLE FUNCTIONAL DATA STRUCUTRES
      */
     val secondArray = firstArray.map(i => i + 1)
     secondArray.foreach(i => if(i == secondArray.last) print(s"$i\n") else print(s"$i,"))
